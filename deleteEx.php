17f8cf868112f6100e49952011829ead12118efe
Dinara-Nassyrova <dinara.nassyrova@is.sdu.edu.kz>
2016-05-14 15:24:53 +0600

    Add files via upload

--- deleteEx.php
+++ deleteEx.php
@@ -4,10 +4,13 @@
   if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
+$sqlSecond = "DELETE FROM comments where expe_id = '$var'";
 $sql = "DELETE FROM experience WHERE ex_id='$var'";
-if ($con->query($sql) === TRUE) {
-  header("Location:myexperience.php");
-} else {
+if ($con->query($sqlSecond) === TRUE) {
+	if($con->query($sql)===TRUE){
+		header("Location:myexperience.php");
+	}
+}else {
     header("Location:myexperience.php");
 }
 
